ea987adf1e781774e2f23720323acb1d8def4515
mrbrack <116898280+mrbrack@users.noreply.github.com>
2023-03-19 00:21:41 +0000

    update

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 const Footer = (className) => {
   return (
     <footer className={cl(className, styles.footer)}>
-      <p></p>&copy; Copyright 2023, datarav3. All Rights Reserved.</p>
+      <p>&copy; Copyright 2023, datarav3. All Rights Reserved.</p>
     </footer>
   );
 };
