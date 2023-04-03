8f7c4fe64f99c1ac9c6ca89460a74442216a4de7
mrbrack <116898280+mrbrack@users.noreply.github.com>
2023-04-03 16:05:30 +0100

    Google Analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,10 +2,43 @@ import "@/styles/reset.scss";
 import "@/styles/global.scss";
 import { Layout } from "@/components";
 import Head from "next/head";
+import Script from 'next/script'
+import { useRouter } from 'next/router';
+import { useEffect } from "react";
+import * as gtag from "../lib/gtag"
 
 export default function App({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+    gtag.pageview(url);
+    };
+    
+    router.events.on("routeChangeComplete", handleRouteChange);
+    
+    return () => {
+    router.events.off("routeChangeComplete", handleRouteChange);
+    };
+    }, [router.events]);
+
   return (
     <>
+<Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXX"></Script>
+<Script
+id='google-analytics'
+strategy="afterInteractive"
+dangerouslySetInnerHTML={{
+__html: `
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-XXXXXX', {
+page_path: window.location.pathname,
+});
+`,
+}}
+/>
       <Head>
         <link
           rel="stylesheet"
