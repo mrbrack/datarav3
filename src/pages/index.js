185714f66be963f0804154bd7cc6e79bdc962a68
mrbrack <116898280+mrbrack@users.noreply.github.com>
2023-05-04 15:05:24 +0100

    update header home
    change video to image

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,7 @@ import {
 } from "@/components";
 import { useState } from "react";
 import Head from "next/head";
+import Image from "next/image";
 
 const LOAD_MORE_STEP = 4;
 
@@ -46,38 +47,20 @@ export default function Home({ initialPosts, total }) {
       </Head>
       <SocialNetworks />
       <Section type="top" conditions="lvp">
-        <video
-          poster="/planet_header_lvp_cover.png"
-          width="1300"
-          height="650"
-          className="header-video"
-          autoPlay
-          playsInline
-          muted
-          loop
-        >
-          <source src="/planet_header_lvp.mp4" type="video/mp4" />
-          <source src="/planet_header_lvp.ogv" type="video/ogg" />
-          <source src="/planet_header_lvp.webm" type="video/webm" />
-          Your browser does not support the video tag.
-        </video>
+      <Image
+            src="/header_lvp.jpg"
+            alt="generative wormhole in greyscale with a vintage television/CRT monitor aesthetic."
+            width="1300"
+            height="650"
+            />
       </Section>
       <Section type="top" conditions="svp">
-        <video
-          poster="/planet_header_svp_cover.png"
-          width="1080"
-          height="1920"
-          className="header-video"
-          autoPlay
-          playsInline
-          muted
-          loop
-        >
-          <source src="/planet_header_svp.mp4" type="video/mp4" />
-          <source src="/planet_header_svp.ogv" type="video/ogg" />
-          <source src="/planet_header_svp.webm" type="video/webm" />
-          Your browser does not support the video tag.
-        </video>
+      <Image
+            src="/header_svp.jpg"
+            alt="generative wormhole in greyscale with a vintage television/CRT monitor aesthetic."
+            width="1300"
+            height="650"
+            />
       </Section>
       <Section type="standard">
         <h2>work</h2>
