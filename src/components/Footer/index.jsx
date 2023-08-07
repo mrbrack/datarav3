c4fd7551ee26e832c16f5d6b3efe7eafc2741f76
mrbrack <116898280+mrbrack@users.noreply.github.com>
2023-08-07 23:03:02 +0100

    branding

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -5,8 +5,10 @@ import Link from "next/link";
 const Footer = (className) => {
   return (
     <footer className={cl(className, styles.footer)}>
+      <div>
       <small>&copy; copyright 2023, datarav3. all rights reserved. bristol, uk.</small>
-    </footer>
+      </div>
+      </footer>
   );
 };
 
