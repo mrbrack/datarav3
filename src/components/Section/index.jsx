5f9cff8c115abf11b3cc9346fcbd38920868282e
mrbrack <116898280+mrbrack@users.noreply.github.com>
2023-08-07 13:30:11 +0100

    Autumn update

--- src/components/Section/index.jsx
+++ src/components/Section/index.jsx
@@ -1,13 +1,48 @@
 import cl from "classnames";
 import styles from "./index.module.scss";
+import Image from "next/image";
+import {
+  Title
+} from "@/components";
 
-const Section = ({ children, className, type, conditions}) => {
-  if (type === "standard") {
-    if(conditions) {
+const Section = ({ children, className, type, title, titleType, img, alt}) => {
+
+  if (type === "headerAnimated") {
       return (
-        <section className={cl(className, styles.sectionStandard, conditions)}>
+        <section className={cl(className, styles.sectionHeaderAnimated)}>
+        <h1 className={cl(className, styles.sectionHeaderAnimatedHeading)}> 
+        {children}
+        </h1>
+        <style jsx>{`
+          section {background-image: url(${img})};
+      `}</style>
+        </section>
+      );
+  }
+
+  if (type === "header-page") {
+    return (
+      <section className={cl(className, styles.sectionHeaderPage)}>
+      <h1 className={cl(className, styles.sectionHeaderPageHeading)}> 
+      {children}
+      </h1>
+      <style jsx>{`
+        section {background-image: url(${img})};
+    `}</style>
+      </section>
+    );
+}
+
+
+  if (type === "standard") {
+    if(title) {
+      return (
+        <div className={cl(className, styles.sectionStandardOuter)}>
+        <Title type="standard" title={title} titleTag={titleType}/>
+        <section className={cl(className, styles.sectionStandardTitle)}>
           {children}
         </section>
+        </div>
       ); 
     } else {
       return (
@@ -18,11 +53,11 @@ const Section = ({ children, className, type, conditions}) => {
     }
   }
 
-  if (type === "top") {
 
-    if(conditions) {
+  if (type === "top") {
+    if(title) {
       return (
-        <section className={cl(className, styles.sectionTop, conditions)}>
+        <section className={cl(className, styles.sectionTop)}>
           {children}
         </section>
       ); 
@@ -36,20 +71,39 @@ const Section = ({ children, className, type, conditions}) => {
   }
 
   if (type === "two-column") {
+    if(title) {
+      return (
+        <section className={cl(className, styles.sectionTopTitle)}>
+          {children}
+        </section>
+      ); 
+    } else {
     return (
       <section className={cl(className, styles.sectionTwoColumnTop)}>
         {children}
       </section>
     );
+    }
   }
 
   if (type === "two-column-top") {
+    if(title) {
+      return (
+        <>
+        <Title type="standard" title={title} titleTag={titleType}/>
+        <section className={cl(className, styles.sectionTwoColumnTop)}>
+          {children}
+        </section>
+        </>
+      ); 
+    } else {
     return (
       <section className={cl(className, styles.sectionTwoColumnTop)}>
         {children}
       </section>
     );
   }
+}
 };
 
 export default Section;
---
+++ src/components/Title/index.jsx
@@ -0,0 +1,31 @@
+import cl from "classnames";
+import styles from "./index.module.scss";
+
+const Title = ({ className, title, titleTag }) => {
+    if(titleTag === "h2") {
+      return (
+      <h2 className={cl(className, styles.title)}>{title}</h2>
+    )}
+
+    if(titleTag === "h2Top") {
+      return (
+      <h2 className={cl(className, styles.titleTop)}>{title}</h2>
+    )}
+
+    if(titleTag === "h1") {
+      return (
+      <h1 className={cl(className, styles.title)}>{title}</h1>
+    )}
+
+    if(titleTag === "h1Top") {
+      return (
+      <h1 className={cl(className, styles.titleTop)}>{title}</h1>
+    )}
+
+    if(titleTag === "h1Bottom") {
+      return (
+      <h1 className={cl(className, styles.titleBottom)}>{title}</h1>
+    )}
+};
+
+export default Title;
