30b8148202d534cca13a1c030e781961819c3dd5
mrbrack <116898280+mrbrack@users.noreply.github.com>
2023-08-09 11:13:28 +0100

    strip out google analytics

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,25 +2,10 @@ import "@/styles/reset.scss";
 import "@/styles/global.scss";
 import { Layout } from "@/components";
 import Head from "next/head";
-
 import Script from "next/script"
-import { useRouter } from "next/router"
-import { useEffect } from "react"
 import CookieConsent from "react-cookie-consent";
 
-import * as gtag from "@/lib/gtag.js"
-
 export default function App({ Component, pageProps }) {
-  const router = useRouter()
-  useEffect(() => {
-    const handleRouteChange = url => {
-      gtag.pageview(url)
-    }
-    router.events.on("routeChangeComplete", handleRouteChange)
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange)
-    }
-  }, [router.events])
   return (
     <>
       <Head>
@@ -38,24 +23,6 @@ export default function App({ Component, pageProps }) {
 
       <meta property="og:image" content="/assets/og/og_temp.jpg" key="ogimage" />
       </Head>
-      <Script
-        strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
-      />
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-            gtag('config', '${gtag.GA_TRACKING_ID}', {
-              page_path: window.location.pathname,
-            });
-          `,
-        }}
-      />
       <Layout>
         <Component {...pageProps} />
       </Layout>
